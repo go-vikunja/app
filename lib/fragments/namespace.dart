fb3c6e78ba0d3677ac85ceb240f169f0c97dae4c
Jonas Franz <info@jonasfranz.software>
2018-09-15 17:22:28 +0200

    Reformat code Add "Add namespace" functionality

--- lib/fragments/namespace.dart
+++ lib/fragments/namespace.dart
@@ -4,21 +4,21 @@ class NamespaceFragment extends StatelessWidget {
   final String namespace;
 
   NamespaceFragment({this.namespace});
-  
+
   @override
   Widget build(BuildContext context) {
     return new Center(
         child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Container(
-              child: new Text(
-                'Namespace: $namespace',
-                style: Theme.of(context).textTheme.title,
-              ),
-            ),
-            new Text('You\'ve selected a namespace!')
-          ],
-        ));
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Container(
+          child: new Text(
+            'Namespace: $namespace',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+        new Text('You\'ve selected a namespace!')
+      ],
+    ));
   }
 }
