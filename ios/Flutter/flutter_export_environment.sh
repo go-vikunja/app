6c0762d4585eaf646fb99c015e43675e026893e4
JonasFranz <email@jfdev.de>
2020-06-17 15:54:19 +0000

    Provide build for iOS (#42)
    Remove pipeline for feature/ios
    
    Merge branch 'master' of ssh://git.kolaente.de:9022/vikunja/app into feature/ios
    
    Fix CI
    
    Add keychain pw
    
    ensure keychain
    
    Disable code signing while building
    
    Fix CI
    
    Merge branch 'feature/ios' of ssh://git.kolaente.de:9022/vikunja/app into feature/ios
    
    Add keychain password
    
    Merge branch 'master' into feature/ios
    
    Add compliance
    
    Add secrets
    
    Remove build app step
    
    Use other version
    
    Set Utf8
    
    Fix CI
    
    Fix CI
    
    Fix beta deployment
    
    Add deploy step
    
    Fix CI
    
    Fix CI
    
    Fix ci
    
    fix ci
    
    fix ci
    
    test ci
    
    fix ci
    
    Fix keychain
    
    Fix keychain
    
    Use custom keychain
    
    Add security unlock
    
    Add MATCH_PASSWORD
    
    Add match
    
    Add fastlane
    
    Add ios pipeline to build
    
    Add ios pipeline to build
    
    Co-authored-by: Jonas Franz <info@jonasfranz.software>
    Co-authored-by: Buildslave <buildslave@macmini.fritz.box>
    Reviewed-on: https://kolaente.dev/vikunja/app/pulls/42
    Reviewed-by: konrad <konrad@kola-entertainments.de>

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/nix/store/nyvp7jf7sfxsbc6jsm1y4fc18631ap26-flutter-stable-1.12.13+hotfix.5-unwrapped"
-export "FLUTTER_APPLICATION_PATH=/home/konrad/www/vikunja/app2"
+export "FLUTTER_ROOT=/opt/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/jonasfranz/Projects/vikunja/app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/konrad/.cache/flutter/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/opt/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.1.0"
 export "FLUTTER_BUILD_NUMBER=0.1.0"
