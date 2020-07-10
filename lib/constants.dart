8f595c340f712ba9e765a4f904ccb0a787b53099
kolaente <k@knt.li>
2020-07-10 10:32:16 +0200

    Revert "Add sentry (#43)"
    This reverts commit 9c2622e7

--- lib/constants.dart
+++
@@ -1,2 +0,0 @@
-const SENTRY_DSN =
-    'https://b070ed4bd1d043428db6fe7d1ce57908@sentry.kolaente.de/5';
--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,10 @@
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sentry/sentry.dart';
-import 'package:vikunja_app/constants.dart';
 import 'package:vikunja_app/global.dart';
 import 'package:vikunja_app/pages/home.dart';
 import 'package:vikunja_app/pages/user/login.dart';
 import 'package:vikunja_app/theme/theme.dart';
 
-void main() {
-  if (!kReleaseMode) {
-    // only log errors in release mode
-    _startApp();
-    return;
-  }
-  var sentry = new SentryClient(dsn: SENTRY_DSN);
-
-  FlutterError.onError = (details, {bool forceReport = false}) {
-    try {
-      sentry.captureException(
-        exception: details.exception,
-        stackTrace: details.stack,
-      );
-    } catch (e) {
-      print('Sending report to sentry.io failed: $e');
-    } finally {
-      // Also use Flutter's pretty error logging to the device's console.
-      FlutterError.dumpErrorToConsole(details, forceReport: forceReport);
-    }
-  };
-
-  runZoned(
-    _startApp,
-    onError: (Object error, StackTrace stackTrace) {
-      try {
-        sentry.captureException(
-          exception: error,
-          stackTrace: stackTrace,
-        );
-        print('Error sent to sentry.io: $error');
-      } catch (e) {
-        print('Sending report to sentry.io failed: $e');
-        print('Original error: $error');
-      }
-    },
-  );
-}
-
-_startApp() => runApp(VikunjaGlobal(
+void main() => runApp(VikunjaGlobal(
     child: new VikunjaApp(home: HomePage()),
     login: new VikunjaApp(home: LoginPage())));
 
