a7be6abd0d5165111c173b1fa9f91bccfb99359f
Paul Nettleton <k9@k9withabone.dev>
2022-09-04 15:33:29 -0500

    tweak validator.dart to avoid regex calls if null

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -2,12 +2,14 @@ final RegExp _emailRegex = new RegExp(
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
 
 bool isEmail(String? email) {
-  return _emailRegex.hasMatch(email ?? '');
+  if (email == null) return false;
+  return _emailRegex.hasMatch(email);
 }
 
 final RegExp _url = new RegExp(
     r'https?:\/\/((([a-zA-Z0-9.\-\_]+)\.[a-zA-Z]+)|(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))(:[0-9]+)?');
 
 bool isUrl(String? url) {
-  return _url.hasMatch(url ?? '');
+  if (url == null) return false;
+  return _url.hasMatch(url);
 }
