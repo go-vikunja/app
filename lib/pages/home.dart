fb5b2de493ea55b7c118bd304fa908b4ba8de7df
Denys Vitali <denys@denv.it>
2024-03-26 22:15:06 +0100

    refactor: get rid of unused imports, update deps

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:vikunja_app/pages/landing_page.dart';
-import 'package:vikunja_app/global.dart';
 import 'package:vikunja_app/pages/project/overview.dart';
 import 'package:vikunja_app/pages/settings.dart';
 
