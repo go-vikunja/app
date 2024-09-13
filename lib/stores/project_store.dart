b2dcb0fea1cdc222e05f0421e88f7f5777e446f7
Benimautner <benimautner@gmail.com>
2024-09-13 14:52:01 +0200

    fix: if there is no bucket, we don't manually add the task to it.

--- lib/stores/project_store.dart
+++ lib/stores/project_store.dart
@@ -152,9 +152,8 @@ class ProjectProvider with ChangeNotifier {
       }
       if (_tasks.isNotEmpty) _tasks.insert(0, task);
       if (_buckets.isNotEmpty) {
-        final bucket =
-            _buckets[_buckets.indexWhere((b) => task.bucketId == b.id)];
-        bucket.tasks.add(task);
+        final Bucket? bucket = _buckets.where((b) => task.bucketId == b.id).firstOrNull;
+        if (bucket != null) bucket.tasks.add(task);
       }
       pageStatus = PageStatus.success;
     });
