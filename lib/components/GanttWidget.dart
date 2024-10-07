8536942d7a122cebe99c99aaa087c7ff5e59d2e8
Benimautner <benimautner@gmail.com>
2024-10-07 21:20:39 +0200

    feat(gantt): finished implementation of moving task around and dragging start and finish

--- lib/components/GanttWidget.dart
+++ lib/components/GanttWidget.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:vikunja_app/stores/project_store.dart';
 
 import '../models/task.dart';
+import 'TaskBottomSheet.dart';
 
 class GanttWidget extends StatefulWidget {
   @override
@@ -47,8 +48,12 @@ class _GanttWidgetState extends State<GanttWidget> {
 
   late double _taskHeight;
   bool inited = false;
+  int taskDragging = -1;
 
   late ProjectProvider projectState;
+
+  Map<int, double> taskStartOffsets = {};
+  Map<int, double> taskEndOffsets = {};
 
   List<MockTask> tasks = [
     MockTask(
@@ -61,13 +66,18 @@ class _GanttWidgetState extends State<GanttWidget> {
 
   @override
   Widget build(BuildContext context) {
+    _taskHeight = MediaQuery.of(context).size.height / 20;
+    dayWidth = MediaQuery.of(context).size.width / 15;
+
     if (!inited) {
       projectState = Provider.of<ProjectProvider>(context, listen: false);
       for (Task task in projectState.ganttTasks) {
         print(task.toJSON());
+
+        double taskStart =
+            dayWidth * task.startDate!.difference(startDateTime).inDays;
       }
-      _taskHeight = MediaQuery.of(context).size.height / 20;
-      dayWidth = MediaQuery.of(context).size.width / 15;
+
       double initialOffset =
           (DateTime.now().difference(startDateTime)).inDays * dayWidth;
 
@@ -152,27 +162,61 @@ class _GanttWidgetState extends State<GanttWidget> {
                         itemCount: projectState.ganttTasks.length,
                         itemBuilder: (context, index) {
                           Task task = projectState.ganttTasks[index];
+                          double taskStartOffset =
+                              taskStartOffsets.containsKey(task.id)
+                                  ? taskStartOffsets[task.id]!
+                                  : 0;
+                          double taskEndOffset =
+                              taskEndOffsets.containsKey(task.id)
+                                  ? taskEndOffsets[task.id]!
+                                  : 0;
                           double taskStart = dayWidth *
-                              task.startDate!.difference(startDateTime).inDays;
-                          return UnconstrainedBox(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              padding: EdgeInsets.only(left: taskStart),
-                              height: _taskHeight,
-                              width: taskStart +
-                                  dayWidth *
-                                      task.endDate!
-                                          .difference(task.startDate!)
-                                          .inDays,
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                      border: Border.all(color: Colors.red)),
-                                  child: Text(task.title +
-                                      "" +
-                                      task.endDate!
-                                          .difference(task.startDate!)
-                                          .inDays
-                                          .toString())),
+                                  task.startDate!
+                                      .difference(startDateTime)
+                                      .inDays +
+                              taskStartOffset;
+                          double taskEnd =
+                              task.endDate!.difference(task.startDate!).inDays *
+                                      dayWidth -
+                                  taskStartOffset;
+
+                          return Padding(
+                            padding: EdgeInsets.only(left: taskStart),
+                            child: Stack(
+                              children: [
+                                Container(
+                                    height: _taskHeight,
+                                    width: taskEnd + taskEndOffset,
+                                    child: LongPressDraggable(
+                                        onDragEnd: (details) {
+                                          taskDragging = -1;
+                                          DateTime newStartDate =
+                                              startDateTime.add(Duration(
+                                                  days: ((details.offset.dx +
+                                                              _taskScrollController
+                                                                  .offset) /
+                                                          dayWidth)
+                                                      .round()));
+                                          task.endDate = task.endDate!.add(
+                                              newStartDate
+                                                  .difference(task.startDate!));
+                                          task.startDate = newStartDate;
+                                        },
+                                        onDragStarted: () {
+                                          taskDragging = task.id;
+                                        },
+                                        axis: Axis.horizontal,
+                                        feedback: SizedBox(
+                                            width: task.endDate!
+                                                    .difference(task.startDate!)
+                                                    .inDays *
+                                                dayWidth,
+                                            height: _taskHeight,
+                                            child: buildCard(task)),
+                                        child: taskDragging != task.id
+                                            ? buildCard(task)
+                                            : Container()))
+                              ],
                             ),
                           );
                         },
@@ -183,32 +227,75 @@ class _GanttWidgetState extends State<GanttWidget> {
               ],
             ),
           ),
-          /*Container(
-            height: MediaQuery.of(context).size.height / 8,
-            width: MediaQuery.of(context).size.width * 10,
-            child: Flex(
-              direction: Axis.horizontal,
-              children: [
-                SingleChildScrollView(
-                controller: _taskScrollController,
-                scrollDirection: Axis.horizontal,
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      children: [
-                        Text("1"),
-                        Text("1"),
-                        Text("1"),
-                        Text("1"),
-                      ],
-                    )
-                  ),
-              ),
-            ]
-            ),
-          )*/
         ],
       ),
     );
+  }
+
+  Widget buildCard(Task task) {
+    return Card(
+        key: ValueKey(task.id),
+        child: GestureDetector(
+          onTap: () {
+            showModalBottomSheet<void>(
+                context: context,
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(10.0)),
+                ),
+                builder: (BuildContext context) {
+                  return TaskBottomSheet(
+                      task: task, onEdit: () => {}, taskState: projectState);
+                });
+          },
+          child: Row(
+            children: [
+              // handle at start to drag startDate
+              LongPressDraggable(
+                  onDragUpdate: (details) {
+                    setState(() {
+                      taskStartOffsets[task.id] = details.delta.dx +
+                          (taskStartOffsets.containsKey(task.id)
+                              ? taskStartOffsets[task.id]!
+                              : 0);
+                    });
+                  },
+                  child: Container(child: Icon(Icons.drag_indicator)),
+                  feedback: SizedBox(),
+                  onDragEnd: (details) {
+                    setState(() {
+                      task.startDate = task.startDate!.add(Duration(
+                          days: (taskStartOffsets[task.id]!) ~/ dayWidth));
+                      projectState.updateTask(context: context, task: task);
+                      taskStartOffsets[task.id] = 0;
+                    });
+                  },
+                  axis: Axis.horizontal),
+
+              Expanded(child: Text(task.title)),
+              LongPressDraggable(
+                  onDragUpdate: (details) {
+                    setState(() {
+                      taskEndOffsets[task.id] = details.delta.dx +
+                          (taskEndOffsets.containsKey(task.id)
+                              ? taskEndOffsets[task.id]!
+                              : 0);
+                    });
+                  },
+                  child: Container(child: Icon(Icons.drag_indicator)),
+                  feedback: SizedBox(),
+                  onDragEnd: (details) {
+                    setState(() {
+                      task.endDate = task.endDate!.add(Duration(
+                          days: (taskEndOffsets[task.id]!) ~/ dayWidth));
+                      projectState.updateTask(context: context, task: task);
+                      taskEndOffsets[task.id] = 0;
+                    });
+                    print("drag end");
+                  },
+                  axis: Axis.horizontal),
+            ],
+          ),
+        ));
   }
 }
