20a2e8ebad501820293b9285de578d684d1caecd
kolaente <k@knt.li>
2025-08-05 21:38:13 +0200

    chore: update devenv

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
 
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
 {
   enterShell = ''
     export CHROME_EXECUTABLE=`which chromium`
@@ -7,22 +10,26 @@
 
   scripts = {
     create-emulator.exec = "avdmanager create avd --force --name android-32 --package 'system-images;android-32;google_apis_playstore;x86_64'";
-    run-app.exec = "flutter run --flavor unsigned";
-    build-apk-unsigned.exec = "flutter build apk --flavor unsigned";
+    run-app.exec = "flutter run";
+    build-apk-unsigned.exec = "flutter build apk";
     lint.exec = "dart format --set-exit-if-changed .";
     lint-fix.exec = "dart format .";
   };  
 
   android = {
     enable = true;
-    flutter.enable = true;
+    flutter = {
+      enable = true;
+      package = pkgs-unstable.flutter;
+    };
 
-    platforms.version = [ "31" "33" "34" ];
+    platforms.version = [ "31" "33" "34" "35" ];
+    buildTools.version = [ "34.0.0" ];
     cmake.version = [ "3.18.1" "3.22.1" ];
     googleTVAddOns.enable = false;
     ndk = {
       enable = true;
-      version = [ "23.1.7779620" "26.1.10909125" ];
+      version = [ "23.1.7779620" "26.3.11579264" "27.0.12077973" ];
     };
     extras = [ ];
     emulator = {
