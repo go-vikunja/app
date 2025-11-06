974e87f7b95c4a3d5a1fe1e7ef83e0b0c56b628d
Benedikt <lesch.benedikt@gmail.com>
2025-11-06 08:59:09 +0100

    fix: task item spacing

--- lib/presentation/widgets/project/project_task_list_item.dart
+++ lib/presentation/widgets/project/project_task_list_item.dart
@@ -26,6 +26,8 @@ class ProjectTaskListItemState extends State<ProjectTaskListItem> {
 
   @override
   Widget build(BuildContext context) {
+    var subtitle = _buildTaskSubtitle(widget.task, context);
+
     return Stack(
       fit: StackFit.loose,
       children: [
@@ -38,7 +40,7 @@ class ProjectTaskListItemState extends State<ProjectTaskListItem> {
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
           ),
-          subtitle: _buildTaskSubtitle(widget.task, context),
+          subtitle: subtitle,
           leading: Checkbox(
             value: widget.task.done,
             onChanged: (bool? newValue) {
@@ -52,7 +54,7 @@ class ProjectTaskListItemState extends State<ProjectTaskListItem> {
             onPressed: () => widget.onEdit(),
           ),
         ),
-        Container(width: 4.0, height: 72.0, color: widget.task.color),
+        Container(width: 4.0, height: subtitle != null ? 68.0 : 56.0, color: widget.task.color),
       ],
     );
   }
@@ -76,14 +78,9 @@ class ProjectTaskListItemState extends State<ProjectTaskListItem> {
       return null;
     }
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
-          child: Row(children: texts),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
+      child: Row(children: texts),
     );
   }
 }
