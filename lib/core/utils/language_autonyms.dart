3c70742c435fbf7ce25f1020aaf93ef6e3b03eb2
RXTX4816 <RXTX4816@local.local>
2025-12-05 21:05:06 +0100

    refactor(utils): remove 3rd party autonym lib

--- lib/core/utils/language_autonyms.dart
+++ lib/core/utils/language_autonyms.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 import 'package:intl/intl.dart';
+
+const Map<String, String> kLocaleAutonyms = <String, String>{
+  'en': 'English',
+  'en_US': 'English (United States)',
+  'en_GB': 'English (United Kingdom)',
+  'es': 'español',
+  'es_ES': 'español (España)',
+  'pl': 'polski',
+  'pl_PL': 'polski (Polska)',
+  'de': 'Deutsch',
+  'de_DE': 'Deutsch (Deutschland)',
+  'fr': 'français',
+  'fr_FR': 'français (France)',
+  'pt': 'português',
+  'pt_PT': 'português (Portugal)',
+  'pt_BR': 'português (Brasil)',
+  'it': 'italiano',
+  'it_IT': 'italiano (Italia)',
+  'nl': 'Nederlands',
+  'nl_NL': 'Nederlands (Nederland)',
+  'ru': 'русский',
+  'ru_RU': 'русский (Россия)',
+  'uk': 'українська',
+  'uk_UA': 'українська (Україна)',
+  'tr': 'Türkçe',
+  'tr_TR': 'Türkçe (Türkiye)',
+  'el': 'Ελληνικά',
+  'el_GR': 'Ελληνικά (Ελλάδα)',
+  'cs': 'čeština',
+  'cs_CZ': 'čeština (Česká republika)',
+  'sk': 'slovenčina',
+  'sk_SK': 'slovenčina (Slovensko)',
+  'sv': 'svenska',
+  'sv_SE': 'svenska (Sverige)',
+  'da': 'dansk',
+  'da_DK': 'dansk (Danmark)',
+  'nb': 'norsk bokmål',
+  'nb_NO': 'norsk bokmål (Norge)',
+  'fi': 'suomi',
+  'fi_FI': 'suomi (Suomi)',
+  'hu': 'magyar',
+  'hu_HU': 'magyar (Magyarország)',
+  'ro': 'română',
+  'ro_RO': 'română (România)',
+  'bg': 'български',
+  'bg_BG': 'български (България)',
+  'sr_Latn': 'srpski (latinica)',
+  'sr_Cyrl': 'српски (ћирилица)',
+  'hr': 'hrvatski',
+  'hr_HR': 'hrvatski (Hrvatska)',
+  'bs': 'bosanski',
+  'bs_BA': 'bosanski (Bosna i Hercegovina)',
+  'sl': 'slovenščina',
+  'sl_SI': 'slovenščina (Slovenija)',
+  'lt': 'lietuvių',
+  'lt_LT': 'lietuvių (Lietuva)',
+  'lv': 'latviešu',
+  'lv_LV': 'latviešu (Latvija)',
+  'et': 'eesti',
+  'et_EE': 'eesti (Eesti)',
+};
 
 String languageAutonym(Locale locale) {
   final canonicalLocale = Intl.canonicalizedLocale(locale.toString());
-  final nativeNames = LocaleNamesLocalizationsDelegate.nativeLocaleNames;
+  final nativeNames = kLocaleAutonyms;
 
   final autonym =
       nativeNames[canonicalLocale] ??
