b5d8ca635f34a443411561b55ac0513f756f63e5
Benedikt <lesch.benedikt@gmail.com>
2026-02-03 13:44:06 +0100

    fix: missing translation of repeat after dropdown

--- lib/core/utils/repeat_after_parse.dart
+++ lib/core/utils/repeat_after_parse.dart
@@ -1,39 +1,50 @@
-String? getRepeatAfterTypeFromDuration(Duration? repeatAfter) {
-  if (repeatAfter == null || repeatAfter.inSeconds == 0) {
-    return null;
-  }
+import 'package:flutter/cupertino.dart';
+import 'package:vikunja_app/l10n/gen/app_localizations.dart';
 
-  // if its dividable by 24, its something with days, otherwise hours
-  if (repeatAfter.inHours % 24 == 0) {
-    if (repeatAfter.inDays % 365 == 0) {
-      return 'Years';
-    } else if (repeatAfter.inDays % 30 == 0) {
-      return 'Months';
-    } else if (repeatAfter.inDays % 7 == 0) {
-      return 'Weeks';
-    } else {
-      return 'Days';
-    }
-  }
-  return 'Hours';
+List<String> getRepeatAfterArray(BuildContext context) {
+  var localizations = AppLocalizations.of(context);
+  return <String>[
+    localizations.repeatUnitHours,
+    localizations.repeatUnitDays,
+    localizations.repeatUnitWeeks,
+    localizations.repeatUnitMonths,
+    localizations.repeatUnitYears,
+  ];
 }
 
-int? getRepeatAfterValueFromDuration(Duration? repeatAfter) {
+int getRepeatAfterTypeFromDuration(Duration? repeatAfter) {
   if (repeatAfter == null || repeatAfter.inSeconds == 0) {
-    return null;
+    return 0;
   }
 
   // if its dividable by 24, its something with days, otherwise hours
   if (repeatAfter.inHours % 24 == 0) {
     if (repeatAfter.inDays % 365 == 0) {
-      // Years
-      return (repeatAfter.inDays / 365).round();
+      return 4; //Years
     } else if (repeatAfter.inDays % 30 == 0) {
-      // Months
-      return (repeatAfter.inDays / 30).round();
+      return 3; //Months
     } else if (repeatAfter.inDays % 7 == 0) {
-      // Weeks
-      return (repeatAfter.inDays / 7).round();
+      return 2; //Weeks
+    } else {
+      return 1; //Days
+    }
+  }
+  return 0; //Hours
+}
+
+int getRepeatAfterValueFromDuration(Duration? repeatAfter) {
+  if (repeatAfter == null || repeatAfter.inSeconds == 0) {
+    return 0;
+  }
+
+  // if its dividable by 24, its something with days, otherwise hours
+  if (repeatAfter.inHours % 24 == 0) {
+    if (repeatAfter.inDays % 365 == 0) {
+      return (repeatAfter.inDays / 365).round(); // Years
+    } else if (repeatAfter.inDays % 30 == 0) {
+      return (repeatAfter.inDays / 30).round(); // Months
+    } else if (repeatAfter.inDays % 7 == 0) {
+      return (repeatAfter.inDays / 7).round(); // Weeks
     } else {
       return repeatAfter.inDays; // Days
     }
@@ -43,22 +54,22 @@ int? getRepeatAfterValueFromDuration(Duration? repeatAfter) {
   return repeatAfter.inHours;
 }
 
-Duration? getDurationFromType(int? val, String? type) {
+Duration? getDurationFromType(int val, int type) {
   // Return an empty duration if either of the values is not set
-  if (val == null || val == '' || type == null || type == '') {
+  if (val == '' || type == '') {
     return Duration();
   }
 
   switch (type) {
-    case 'Hours':
+    case 0: //Hours
       return Duration(hours: val);
-    case 'Days':
+    case 1: //Days
       return Duration(days: val);
-    case 'Weeks':
+    case 2: //Weeks
       return Duration(days: val * 7);
-    case 'Months':
+    case 3: //Months
       return Duration(days: val * 30);
-    case 'Years':
+    case 4: //Years
       return Duration(days: val * 365);
   }
 
