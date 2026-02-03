adb259ef123f4bacbdc31afa1368aae691ca9b7d
Benedikt <lesch.benedikt@gmail.com>
2026-02-03 14:33:10 +0100

    fix: fixed test cases

--- test/parse_repeat_after_test.dart
+++ test/parse_repeat_after_test.dart
@@ -5,72 +5,67 @@ void main() {
   test('Repeat after hours', () {
     Duration testDuration = Duration(hours: 6);
 
-    expect(getRepeatAfterTypeFromDuration(testDuration), 'Hours');
+    expect(getRepeatAfterTypeFromDuration(testDuration), 0);
     expect(getRepeatAfterValueFromDuration(testDuration), 6);
   });
 
   test('Repeat after days', () {
     Duration testDuration = Duration(days: 6);
 
-    expect(getRepeatAfterTypeFromDuration(testDuration), 'Days');
+    expect(getRepeatAfterTypeFromDuration(testDuration), 1);
     expect(getRepeatAfterValueFromDuration(testDuration), 6);
   });
 
   test('Repeat after weeks', () {
     Duration testDuration = Duration(days: 6 * 7);
 
-    expect(getRepeatAfterTypeFromDuration(testDuration), 'Weeks');
+    expect(getRepeatAfterTypeFromDuration(testDuration), 2);
     expect(getRepeatAfterValueFromDuration(testDuration), 6);
   });
 
   test('Repeat after months', () {
     Duration testDuration = Duration(days: 6 * 30);
 
-    expect(getRepeatAfterTypeFromDuration(testDuration), 'Months');
+    expect(getRepeatAfterTypeFromDuration(testDuration), 3);
     expect(getRepeatAfterValueFromDuration(testDuration), 6);
   });
 
   test('Repeat after years', () {
     Duration testDuration = Duration(days: 6 * 365);
 
-    expect(getRepeatAfterTypeFromDuration(testDuration), 'Years');
+    expect(getRepeatAfterTypeFromDuration(testDuration), 4);
     expect(getRepeatAfterValueFromDuration(testDuration), 6);
   });
 
   test('Repeat null value', () {
     Duration testDuration = Duration();
 
-    expect(getRepeatAfterTypeFromDuration(testDuration), null);
-    expect(getRepeatAfterValueFromDuration(testDuration), null);
+    expect(getRepeatAfterTypeFromDuration(testDuration), 0);
+    expect(getRepeatAfterValueFromDuration(testDuration), 0);
   });
 
   test('Hours to duration', () {
-    Duration? parsedDuration = getDurationFromType(6, 'Hours');
+    Duration? parsedDuration = getDurationFromType(6, 0);
     expect(parsedDuration, Duration(hours: 6));
   });
 
   test('Days to duration', () {
-    Duration? parsedDuration = getDurationFromType(6, 'Days');
+    Duration? parsedDuration = getDurationFromType(6, 1);
     expect(parsedDuration, Duration(days: 6));
   });
 
   test('Weeks to duration', () {
-    Duration? parsedDuration = getDurationFromType(6, 'Weeks');
+    Duration? parsedDuration = getDurationFromType(6, 2);
     expect(parsedDuration, Duration(days: 6 * 7));
   });
 
   test('Months to duration', () {
-    Duration? parsedDuration = getDurationFromType(6, 'Months');
+    Duration? parsedDuration = getDurationFromType(6, 3);
     expect(parsedDuration, Duration(days: 6 * 30));
   });
 
   test('Years to duration', () {
-    Duration? parsedDuration = getDurationFromType(6, 'Years');
+    Duration? parsedDuration = getDurationFromType(6, 4);
     expect(parsedDuration, Duration(days: 6 * 365));
-  });
-
-  test('null to duration', () {
-    Duration? parsedDuration = getDurationFromType(null, null);
-    expect(parsedDuration, Duration());
   });
 }
