24a5c6f7bbb6f160fc6cd3303781e11105cccc47
Benard <benokolie@hotmail.co.uk>
2026-05-16 19:11:13 +0100

    feat: add button on widget to add a task quickly
    Currently this button opens the main app and launches the add-task
    popup. The only downside right now is if the app is closed and has to be
    opened from the widget it can take a few seconds. Ideally a small/custom
    activity can be created and launched but that is more complex and for
    another day.
    
    Co-authored-by: Solidus <9112726+leschbenedikt@users.noreply.github.com>

--- android/app/src/main/kotlin/io/vikunja/flutteringvikunja/VikunjaTileService.kt
+++ android/app/src/main/kotlin/io/vikunja/flutteringvikunja/VikunjaTileService.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.app.PendingIntent
 import android.os.Build
 import android.service.quicksettings.TileService
-import android.util.Log
 import androidx.annotation.RequiresApi
 
 const val INTENT_TYPE_ADD_TASK = "ADD_NEW_TASK"
